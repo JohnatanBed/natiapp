8a1948da184e8f85807660865fd4331a146db924
Johnatan Bedoya <johnatan.bedoya@outlook.com>
2025-09-10 23:59:16 -0500

    Delete backend directory

--- backend/src/routes/setupRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createFirstAdmin } = require('../controllers/setupController');
-
-const router = express.Router();
-
-// Public route to create the first admin (for setup only)
-router.post('/create-admin', createFirstAdmin);
-
-module.exports = router;
